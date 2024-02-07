0824671f5e229b4ab6c2f29ff72a664bedae372c
piacib <ben.piacitelli@gmail.com>
2024-02-07 14:35:47 -0800

    added comments

--- src/app/borderQuiz/page.tsx
+++ src/app/borderQuiz/page.tsx
@@ -11,24 +11,35 @@ type Country = {
   id: string;
   updated: string;
 };
+// Define a custom type DataReturn which is an array of Country objects and RecordModel objects
 type DataReturn = Country[] & RecordModel[];
 
+// Function to fetch countries data
 async function getCountries() {
+  // Instantiate PocketBase client with the URL
   const pb = new PocketBase("http://127.0.0.1:8090");
 
-  //  fetch all records at once
+  // Fetch all records from the "country_borders" collection sorted by "created" field in descending order
   const records = await pb.collection("country_borders").getFullList({
     sort: "-created",
   });
+
+  // Extract unique country names from the fetched records
   const countryNames = new Set(
     records.map((entry) => entry.country_name)
   ) as Set<string>;
+
+  // Return an array containing unique country names and fetched records
   return [[...countryNames], records] as [any, DataReturn];
 }
+
+// Function to get bordering countries for a given country name
 const getBorderingCountries = (name: string, data: Country[]): string[] => {
   const borderingCountries = data.filter((x) => x.country_border_name == name);
   return borderingCountries.map((x) => x.country_name);
 };
+
+// Function to get additional countries for multiple-choice questions
 const getAdditionalCountries = (
   borderingCountries: string[],
   data: Country[],
@@ -36,29 +47,41 @@ const getAdditionalCountries = (
 ): string[] => {
   const multipleChoiceRequirement = borderingCountries.length * 3;
   const additionalCountries: string[] = [];
-  // Iterate over border countries to get list of their border countries
+
+  // Iterate over bordering countries to get their bordering countries
   const addNewCountries = (countryCode: string) => {
     const newCountryCodes = getBorderingCountries(countryCode, data);
-    // filter out already included countries and the answer country
+
+    // Filter out already included countries, other bordering countries, and the answer country
     const uniqueCountryCodes = newCountryCodes.filter(
-      (x) => !additionalCountries.includes(x) && x != country
+      (x) =>
+        !borderingCountries.includes(x) &&
+        !additionalCountries.includes(x) &&
+        x != country
     );
-    // push new couuntries to additionalCountries
+
+    // Add new countries to additionalCountries
     additionalCountries.push(...uniqueCountryCodes);
   };
+
   borderingCountries.forEach((borderCountry) => {
     addNewCountries(borderCountry);
   });
+
+  // If additional countries meet the requirement, return them
   if (additionalCountries.length >= multipleChoiceRequirement) {
     return additionalCountries;
   }
-  // expand selection to have unique choices for MC
+
+  // Expand selection to have unique choices for multiple-choice questions
   additionalCountries.forEach((x) => {
     addNewCountries(x);
   });
 
   return additionalCountries;
 };
+
+// Function to randomize array elements
 const randomizeArray = (arr: any[]) => {
   const returnArr = arr.sort(func);
 
@@ -67,15 +90,19 @@ const randomizeArray = (arr: any[]) => {
   }
   return returnArr;
 };
+
+// Function to generate question order
 const generateQuestionOrder = (
   borderingCountries: string[],
   additionalCountries: string[]
 ): string[][] => {
   let count = 0;
   const questions: string[][] = [];
+
   borderingCountries.forEach((answerCountry) => {
     const questionSet: string[] = [];
     const questionOrder = randomizeArray([0, 1, 2, 3]);
+
     questionOrder.forEach((x) => {
       if (x == 3) {
         questionSet.push(answerCountry);
@@ -83,13 +110,17 @@ const generateQuestionOrder = (
         if (x + count > additionalCountries.length - 1) {
           count = 0;
         }
-        questionSet.push(additionalCountries[x + count++]);
+        questionSet.push(additionalCountries[count++]);
       }
     });
+
     questions.push(questionSet);
   });
+
   return questions;
 };
+
+// Function to generate question data
 const generateQuestionData = (countryNames: string[], data: Country[]) => {
   const rndIdx = Math.floor(Math.random() * countryNames.length);
   const country = countryNames[rndIdx];
@@ -103,6 +134,7 @@ const generateQuestionData = (countryNames: string[], data: Country[]) => {
   return { country, borderingCountries, additionalCountries };
 };
 
+// Asynchronous function to render the page
 const page = async () => {
   const data = await getCountries();
   const { country, borderingCountries, additionalCountries } =
@@ -111,6 +143,8 @@ const page = async () => {
     borderingCountries,
     additionalCountries
   );
+
+  // Return JSX to render the page
   return (
     <div>
       <div>{JSON.stringify(country)}</div>
@@ -129,6 +163,7 @@ const page = async () => {
           <br />
         </>
       ))}
+      {/* Uncomment the following section to display additional country names */}
       {/* 
       <div>
         {JSON.stringify(additionalCountries.map((x) => x.country_name))}
